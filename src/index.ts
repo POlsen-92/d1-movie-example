6c75a063694876c057d84f73966876d2eb92cec3
Paige Olsen <88565036+POlsen-92@users.noreply.github.com>
2025-10-31 12:10:48 -0700

    Complete

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
-// GET /movies => return all movies
+// GET /movies => return all movies --DONE
 app.get("/movies", async c => {
 	const resp = await c.env.DB
 		.prepare("select * from movies")
@@ -30,18 +30,7 @@ app.get("/movies", async c => {
 	return c.json(movies)
 })
 
-// GET /movies/:id => return individual movie
-app.get("/movies/:id", async c => {
-	const resp = await c.env.DB
-		.prepare("select * from movies WHERE id = ?")
-		.run()
-
-	const movies = resp.results
-
-	return c.json(movies)
-})
-
-// GET /movies/favorites => return 3 favorite movies (sorted by rating)
+// GET /movies/favorites => return 3 favorite movies (sorted by rating) --DONE
 app.get("/movies/favorites", async c => {
 	const resp = await c.env.DB
 		.prepare("select * from movies order by rating desc limit 3")
@@ -52,14 +41,57 @@ app.get("/movies/favorites", async c => {
 	return c.json(movies)
 })
 
-//PUT /movies/:id => re-rate a movie
-app.put("/movies/:id", async c =>{
+// GET /movies/:id => return individual movie --DONE
+app.get("/movies/:id", async c => {
+	const resp = await c.env.DB
+		.prepare("select * from movies WHERE id = ?1")
+		.bind(c.req.param("id"))
+		.run()
 
+	const movie = resp.results
+
+	return c.json(movie)
 })
 
-//POST /movie => add a new movie
-app.post("/movie", async c => {
+// PUT /movies/:id => re-rate a movie --DONE
+app.put("/movies/:id", async c =>{
+	const data = await c.req.json()
+	
+	const resp = await c.env.DB
+		.prepare("UPDATE movies SET rating = ?2, title = ?3, release_date = ?4 where id = ?1 RETURNING *")
+		.bind(c.req.param("id"), data.rating, data.title, data.release_date)
+		.run()
 
+	const ok = resp.success
+
+	return c.json({ ok })
+})
+
+// POST /movie => add a new movie --DONE
+app.post("/movies", async c => {
+	const data = await c.req.json()
+	const {title, release_date, rating} = data
+
+	const resp = await c.env.DB
+		.prepare("INSERT INTO movies (title, release_date, rating) VALUES (?, ?, ?) RETURNING *")
+		.bind(title, release_date, rating)
+		.run()
+
+	const ok = resp.success
+
+	return c.json({ ok })
+})
+
+//DELETE /movies/:id => delete a movie
+app.delete("/movies/:id", async c =>{
+	const resp = await c.env.DB
+		.prepare("DELETE FROM movies WHERE id = ? RETURNING *")
+		.bind(c.req.param("id"))
+		.run()
+
+	const ok = resp.success
+
+	return c.json({ ok })
 })
 
 export default app
